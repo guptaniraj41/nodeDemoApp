3d629b5ad9d1d4e3e900e926a0d28e5b29d299c2
niraj-gupta <niraj.gupta@metacube.com>
2020-07-12 16:11:37 +0530

    [add] code to parse url and get path from request

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 const http = require('http');
+const url = require('url');
 
 var server = http.createServer(function(request, respone) {
+  var parsedUrl = url.parse(request.url, true);
+  var path = parsedUrl.pathname;
+  var trimmedPath = path.replace(/^\/+|\/+$/g, '');
   respone.end('Hello world..!!\n');
+  console.log('Request received on path: ' + trimmedPath);
 });
 
 server.listen(3000, function() {
